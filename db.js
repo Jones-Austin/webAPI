75dc5090ce61aeeb36eadddbf6d8866073f9b590
Jones-Austin <amjones1@pointpark.edu>
2025-04-18 12:52:23 -0400

    updates db.js to connect to remote db

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'Cars',
+    user: 'austin',
+    host: 'dpg-d017uqa4d50c73as3q30-a',
+    database: 'cars_cn15',
     port: 5432,
-    password: 'austin12'
+    password: 'H2eIFey1XEhawQ4bgFqeqrVvA65OyVYW'
 });
 
 
